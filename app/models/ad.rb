348fdd3b2db84a8041427593b6d5aa8dfa7baf90
Jon Ho <jonwho.contact@gmail.com>
2018-01-20 13:17:53 -0800

    callback to create qr_code if created

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -2,4 +2,15 @@ class Ad < ApplicationRecord
   serialize :qr_code
 
   validates :name, uniqueness: { case_sensitive: false }, presence: true
+
+  after_create :generate_qr_code
+
+  private
+
+  def generate_qr_code
+    uuid    = SecureRandom.uuid
+    qr_code = RQRCode::QRCode.new(uuid)
+
+    update_attribute(:qr_code, qr_code)
+  end
 end
